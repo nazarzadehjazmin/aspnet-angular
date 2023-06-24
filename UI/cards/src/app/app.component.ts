6a250dce6f3d0e32e1e8d3877dd41cdc961694a9
nazarzadehjazmin <nazarzadehjazmin@gmail.com>
2023-06-23 23:46:24 -0300

    addcard()

--- UI/cards/src/app/app.component.ts
+++ UI/cards/src/app/app.component.ts
@@ -10,6 +10,14 @@ import { Card } from './models/card.model';
 export class AppComponent implements OnInit {
   title = 'cards';
   cards: Card[] = [];
+  card: Card = {
+    id: '',
+    cardNumber: '',
+    cardHolderName: '',
+    expiryMonth: '',
+    expiryYear: '',
+    cvc: ''
+  }
 
   constructor(private cardService: CardsService) { }
   //use this card service when the page first time loads
@@ -24,6 +32,23 @@ export class AppComponent implements OnInit {
           this.cards = response;
         }
       );
+  }
+
+  onSubmit() {
+    this.cardService.addCard(this.card).subscribe(
+      response => {
+        this.getAllCards();
+        //clear form
+        this.card = {
+          id: '',
+          cardNumber: '',
+          cardHolderName: '',
+          expiryMonth: '',
+          expiryYear: '',
+          cvc: ''
+        };
+      }
+    );
   }
 
 }
--- UI/cards/src/app/services/cards.service.ts
+++ UI/cards/src/app/services/cards.service.ts
@@ -10,12 +10,18 @@ export class CardsService {
 
   baseUrl: string = 'https://localhost:7052/api/Cards'
 
-  constructor(private http : HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   // GetAllCards
   //match with the endpoint of GetAllCards
-  getAllCards(): Observable<Card[]>
-  {
+  getAllCards(): Observable<Card[]> {
     return this.http.get<Card[]>(this.baseUrl);
   }
+
+  addCard(card: Card): Observable<Card> {
+    //[FromBody] = card: Card
+    card.id = '00000000-0000-0000-0000-000000000000';
+    return this.http.post<Card>(this.baseUrl, card);
+  }
+
 }
