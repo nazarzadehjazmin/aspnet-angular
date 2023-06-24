6071baf853df275f8bef8d57d0d706ce13220532
nazarzadehjazmin <nazarzadehjazmin@gmail.com>
2023-06-23 23:07:41 -0300

    Service done

--- UI/cards/src/app/services/cards.service.ts
+++ UI/cards/src/app/services/cards.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Card } from '../models/card.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CardsService {
 
-  constructor() { }
+  baseUrl: string = 'http://localhost:4200/api/cards'
+
+  constructor(private http : HttpClient) { }
+
+  // GetAllCards
+  //match with the endpoint of GetAllCards
+  getAllCards(): Observable<Card[]>
+  {
+    return this.http.get<Card[]>(this.baseUrl);
+  }
 }
